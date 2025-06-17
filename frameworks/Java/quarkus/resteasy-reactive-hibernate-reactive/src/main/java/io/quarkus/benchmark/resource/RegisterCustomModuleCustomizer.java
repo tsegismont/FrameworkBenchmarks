05c423e408e673f205dd857949e2c46e2f9f593c
Thomas Segismont <tsegismo@redhat.com>
2025-06-17 16:53:26 +0200

    Implement Jackson RecyclerPool with FastThreadLocal
    All Quarkus threads are FastThreadLocalThreads.
    
    There is a HybridJacksonPool but for non VT it uses the default pool that is implemented with JDK ThreadLocal.

--- frameworks/Java/quarkus/resteasy-reactive-hibernate-reactive/src/main/java/io/quarkus/benchmark/resource/RegisterCustomModuleCustomizer.java
+++ frameworks/Java/quarkus/resteasy-reactive-hibernate-reactive/src/main/java/io/quarkus/benchmark/resource/RegisterCustomModuleCustomizer.java
@@ -1,7 +1,10 @@
 package io.quarkus.benchmark.resource;
 
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import com.fasterxml.jackson.core.util.RecyclerPool;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import io.netty.util.concurrent.FastThreadLocal;
 import io.quarkus.jackson.ObjectMapperCustomizer;
 import jakarta.inject.Singleton;
 
@@ -10,11 +13,27 @@ public class RegisterCustomModuleCustomizer implements ObjectMapperCustomizer {
 
     public void customize(ObjectMapper mapper) {
         mapper.registerModule(new CustomModule());
+        mapper.getFactory().setRecyclerPool(new FastThreadLocalPool());
     }
 
     private static class CustomModule extends SimpleModule {
         public CustomModule() {
             addSerializer(Message.class, new MessageSerializer());
+        }
+    }
+
+    private static class FastThreadLocalPool extends RecyclerPool.ThreadLocalPoolBase<BufferRecycler> {
+
+        static final FastThreadLocal<BufferRecycler> fastThreadLocal = new FastThreadLocal<>() {
+            @Override
+            protected BufferRecycler initialValue() {
+                return new BufferRecycler();
+            }
+        };
+
+        @Override
+        public BufferRecycler acquirePooled() {
+            return fastThreadLocal.get();
         }
     }
 }
